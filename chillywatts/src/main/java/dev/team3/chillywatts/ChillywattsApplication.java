37b0d3a3f29da3c1414fa72321b3fcc9a1f2407a
Sybilla Coppi <142837986+sybzinha@users.noreply.github.com>
2026-07-08 16:16:02 -0300

    add initial project

---
+++ chillywatts/src/main/java/dev/team3/chillywatts/ChillywattsApplication.java
@@ -0,0 +1,13 @@
+package dev.team3.chillywatts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillywattsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillywattsApplication.class, args);
+	}
+
+}
