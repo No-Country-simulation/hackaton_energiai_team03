fe255d63b7adb11b7b4f7400e48b76d28aa9a8c3
Tiago Flores Lindner <tiago.f.lindner@gmail.com>
2026-07-27 16:48:29 -0300

    limpeza classes inutilizadas

--- chillywatts/src/main/java/dev/team3/chillywatts/ChillywattsApplication.java
+++ chillywatts/src/main/java/dev/team3/chillywatts/ChillywattsApplication.java
@@ -1,9 +1,5 @@
 package dev.team3.chillywatts;
 
-import dev.team3.chillywatts.principal.Principal;
-import dev.team3.chillywatts.repository.FreezerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
